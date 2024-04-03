e4f8b349cb84ac46d46672a66bb0531661c3cbb7
정지은 <kindjjee@gmail.com>
2024-04-03 15:54:17 +0900

    알림 메세지 => 데이터 메세지로 변경, 2가지 상태 모두 알림 수신 가능 (1) 포그라운드 (2) 백그라운드

--- src/main/java/firebase/FirebaseCloudMessageService.java
+++ src/main/java/firebase/FirebaseCloudMessageService.java
@@ -9,17 +9,14 @@ public class FirebaseCloudMessageService {
     // 알림 메시지와 데이터 페이로드 포함
     public static void sendMessage(String registrationToken, String messageData, int messageId, int roomId) throws Exception {
 
-        // 알림 메세지
-        // 어플리케이션이 백그라운드나 종료 상태에 있을때, 시스템이 알림을 자동으로 처리한다는 점이 장점!
-        // 포그라운드에서는 직접 처리
+        // 데이터 메세지
+        // 포그라운드, 백그라운드에서는 직접 일관되게 처리
         Message message = Message.builder() // 메시지 구성을 시작할 수 있는 Builder 객체가 반환
-                .setNotification(Notification.builder()
-                        .setTitle("메세지")
-                        .setBody(messageData)
-                        .build())
-                .putData("message_id", String.valueOf(messageId)) //추가 정보를 데이터 페이로드에 추가
-                .putData("room_id", String.valueOf(roomId))
-                .setToken(registrationToken) // 메서드는 메시지의 수신자를 지정
+                .putData("title", "메세지") // 알림 제목을 데이터 페이로드에 추가
+                .putData("body", messageData) // 메시지 본문을 데이터 페이로드에 추가
+                .putData("messageId", String.valueOf(messageId))// 메세지 id를 데이터 페이로드에 추가
+                .putData("roomId", String.valueOf(roomId)) // room id를 데이터 페이로드에 추가
+                .setToken(registrationToken) // 메시지의 수신자를 지정
                 .build(); // 최종 Message 객체를 생성
 
         // 메시지 보내기
